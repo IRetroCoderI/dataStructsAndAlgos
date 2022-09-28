5a385dcb2f1114fe76890efe979c72fc03da9e6d
Jesus Ramirez <jesusramirez730@gmail.com>
2022-09-28 00:47:45 -0700

    literally added like 3 lines of nothing

--- src/queues/priorityQueues.java
+++ src/queues/priorityQueues.java
@@ -5,5 +5,11 @@ import java.util.Queue;
 public class priorityQueues {
     public static void main(String[] args) {
         Queue<String> line = new PriorityQueue<String>();
+        line.offer("Ben");
+        line.offer("Adam");
+        line.offer("Daniel");
+        line.offer("Chris");
+
+        System.out.println(line.toString());
     }
 }
