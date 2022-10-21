02631dad4e16a5c2c4ece46947d210dfb6dc7e13
Jesus Ramirez <jesusramirez730@gmail.com>
2022-10-20 22:41:09 -0700

    updating

--- src/arrays_and_memory/DynamicArrays/DynamicArray.java
+++ src/arrays_and_memory/DynamicArrays/DynamicArray.java
@@ -17,27 +17,82 @@ public class DynamicArray {
         if(size >= capacity){
             grow();
         }
+        array[size] = data;
+        size++;
 
     }
     public void insert(int index, Object data){
-
+        if(size>=capacity){
+            grow();
+        }
+        for(int i = size; i>index; i--){
+            array[i] = array[i - 1]; //shifts elements to the right, primed for insertion
+        }
+        array[index] = data;
+        size++;
     }
     public void delete(Object data){
+        for(int i = 0; i<size; i++){
+            if(array[i] == data){
+                for(int j = 0; j<(size-i-1); j++){
+                    array[i+j] = array[i + j + 1];
+                }
+                array[size-1] = null;
+                size--;
+                if(size<=(int)(capacity/3)){
+                    shrink();
+                }
+                break;
+            }
+        }
 
     }
     public int search(Object data){
+        for(int i = 0; i<size; i++){
+            if(array[i] == data){
+                return i;
+            }
+        }
         return -1;
     }
     private void grow(){
+        int newCapacity = (int)capacity*2;
+        Object[] newArray = new Object[newCapacity];
 
+        for(int i = 0; i<size; i++){
+            newArray[i] = array[i];
+        }
+        capacity = newCapacity;
+        array = newArray;
+        return;
     }
     private void shrink(){
+        int newCapacity = (int)capacity/2;
+        Object[] newArray = new Object[newCapacity];
 
+        for(int i = 0; i<size; i++){
+            newArray[i] = array[i];
+        }
+        capacity = newCapacity;
+        array = newArray;
+        return;
     }
     public boolean isEmpty(){
         return size == 0;
     }
+
+    @Override
     public String toString(){
-        return null;
+        String string = "";
+        for(int i = 0;i<size;i++){
+            string+=array[i] + ", ";
+        }
+        if(string!=""){
+            string = "["+string.substring(0, string.length() - 2)+ "]";
+        }
+        else{
+            string = "[]";
+        }
+        return string;
     }
 }
