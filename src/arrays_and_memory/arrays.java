3039f5ab8a16c27996589f50ffe6484ed83271ae
Jesus Ramirez <jesusramirez730@gmail.com>
2022-10-20 22:45:01 -0700

    updating v2

--- src/arrays_and_memory/arrays.java
+++ src/arrays_and_memory/arrays.java
@@ -5,6 +5,25 @@ public class arrays {
         int[] myInts = {12, 15, 22, 35};
         String[] names = {"Harry", "Hermoine", "Snape", "Ron", "Neville"};
         char[] alph = {'a', 'b', 'c', 'd', 'e'};
-        System.out.println(myInts[3]);
+ //       System.out.println(myInts[3]);
+
+        int max = 0;
+        int[] nums = {2, 7, 11, 8, 11, 8, 3, 11};
+        for(int i = 0; i<nums.length; i++){
+            for(int j = 1; j<nums.length; j++){
+                System.out.println(nums[i]+ " "+ nums[j] );
+                if(nums[i]<nums[j]){
+                    max = nums[j];
+                }
+            }
+        }
+        System.out.println("Max num is: " + max);
+        int count = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] == max){
+                count++;
+            }
+        }
+        System.out.println("Shows up " + count + " times");
     }
 }
