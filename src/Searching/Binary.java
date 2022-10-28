6a17db4473bd4e40ca79a1309f4f9cc816d82340
Jesus Ramirez <jesusramirez730@gmail.com>
2022-10-28 12:05:19 -0700

    added binary search algo using recursion, will create another without recursion

--- src/Searching/Binary.java
+++ src/Searching/Binary.java
@@ -1,27 +1,34 @@
 package Searching;
+import java.util.Arrays;
 
 public class Binary {
     public static void main(String[] args) {
-        int[] arr = {-2, 3, 4, 7, 8, 9, 11, 13};
-        int target = 4;
-        int elementOfTarget = search(arr, target);
-        System.out.println(elementOfTarget);
+        int[] arr = {1, 3, 4, 7, 8, 9, 11, 13, 20, 44, 52, 55, 60, 300, 405};
+        int target = 11;
 
+
+        System.out.println(binarySearchRecursion(arr, 0, arr.length-1, target));
     }
-    static int search(int[] arr, int target){ //returns index of target
-        int left = 0;
-        int right = arr.length - 1;
-        int mid = 0;
-        while (left <= right){
-            mid = (left + right)/2;
-            if(arr[mid] == target){
-                return mid;
-            } else if (target < arr[mid]) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+
+    public static int binarySearchRecursion(int[] arr, int start, int end, int target){
+
+
+        if(start > end){
+            return -1;
         }
+
+        int mid = (start+end)/2;
+
+
+        if(arr[mid] == target){
+            return mid;
+        } else if(target > arr[mid]){
+            return binarySearchRecursion(arr, mid+1, end, target);
+        } else if(target < arr[mid]){
+            return binarySearchRecursion(arr, start, mid-1, target);
+        }
+
         return -1;
     }
+
 }
--- src/Searching/Linear.java
+++ src/Searching/Linear.java
@@ -5,7 +5,7 @@ public class Linear {
         int[] nums = {1, 3, 5, 6, 3, 6, 67, 34,};
 
         System.out.println(findNum(nums, 00));
-
+        System.out.println(nums.length - 1);
     }
 
     public static int findNum(int[] array, int target) {
