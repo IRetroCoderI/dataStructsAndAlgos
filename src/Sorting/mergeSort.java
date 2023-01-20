d23c54d5c657ca7a3bacfc12f7417cde0d9711be
Jesus Ramirez <jesusramirez730@gmail.com>
2023-01-20 15:06:11 -0800

    updating files

---
+++ src/Sorting/mergeSort.java
@@ -0,0 +1,13 @@
+package Sorting;
+
+public class mergeSort {
+    public static void main(String[] args){
+        int[] myArray = {0, 5, 3, 8, 4, 7, 2, 1};
+    }
+    public static int[] sort(int[] array){
+        return null;
+    }
+    public static int[] merge(int[] array){
+        return null;
+    }
+}
